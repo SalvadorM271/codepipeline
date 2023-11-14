5638e13e3db54cf35759e1b069e6550160b0750a
salvadorM271 <guardianes27@outlook.com>
2023-11-13 22:44:31 -0600

    firts commit

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable bucket_name {
+    default = "test-for-codepipeline-4736477978"
+}
+variable git_branch {
+    default = "testing"
+}
+variable pipeline_name {
+    default = "aws-pipeline"
+}
+//variable git_user {}
+variable git_repo {
+    default = "testmernapp"
+}
+variable codebuild_project_name {
+    default = "codebuild-aws-pipeline"
+}
+variable frontend_repo {
+    default = "438555236323.dkr.ecr.us-east-1.amazonaws.com/ecs_frontend"
+}
+//variable git_email {}
+//variable github_token {}
